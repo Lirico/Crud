44d562d4777a99ed77eeedb37ff0311758acf556
Lirico <dominguezmatiasadrian@gmail.com>
2020-12-04 17:16:24 -0300

    version 4

--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -1,7 +1,13 @@
 import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
-    AGREGAR_PRODUCTO_ERROR
+    AGREGAR_PRODUCTO_ERROR,
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR,
+    OBTENER_PRODUCTO_ELIMINAR,
+    PRODUCTO_ELIMINADO_EXITO,
+    PRODUCTO_ELIMINADO_ERROR 
 
 } from '../types';
 
@@ -9,11 +15,13 @@ import {
 const initialState = {
     productos: [],
     error: null,
-    loading: false
+    loading: false,
+    productoeliminar: null
 }
 
 export default function(state = initialState, action) {
     switch(action.type) {
+        case COMENZAR_DESCARGA_PRODUCTOS:
         case AGREGAR_PRODUCTO:
             return {
                 ...state,
@@ -26,10 +34,23 @@ export default function(state = initialState, action) {
                 productos: [...state.productos, action.payload]
             }
         case AGREGAR_PRODUCTO_ERROR:
+        case DESCARGA_PRODUCTOS_ERROR:    
             return {
                 ...state,
                 loading: false,
                 error: action.payload
+            }
+        case DESCARGA_PRODUCTOS_EXITO:
+            return {
+                ...state,
+                loading: false,
+                error: null,
+                productos: action.payload
+            }    
+        case OBTENER_PRODUCTO_ELIMINAR:
+            return {
+                ...state,
+                productoeliminar: action.payload
             }
 
         default:
